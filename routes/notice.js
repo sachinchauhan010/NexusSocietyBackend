5a00ad65db8e8828638d4298cc2935de772b1a57
Sachin <010sssachin@gmail.com>
2025-04-15 17:49:52 +0530

    find notice by id

--- routes/notice.js
+++ routes/notice.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createNotice, getAllNotices, updateNoticeById, deleteNoticeById } from "../controllers/notice.controller.js";
+import { createNotice, getAllNotices, updateNoticeById, deleteNoticeById, getNoticeById } from "../controllers/notice.controller.js";
 
 
 const noticeRoutes = Router();
 
 noticeRoutes.post('/create-notice', createNotice);
 noticeRoutes.get('/get-notices', getAllNotices);
+noticeRoutes.get('/get-notice/:id', getNoticeById);
 noticeRoutes.put('/update-notice/:id', updateNoticeById);
 noticeRoutes.delete('/delete-notice/:id',deleteNoticeById);
 
