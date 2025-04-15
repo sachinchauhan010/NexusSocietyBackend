a13d5fd41b2c652bb37c3fb4c34fa792357dc427
Sachin <010sssachin@gmail.com>
2025-04-15 16:58:57 +0530

    api created of notice delete and updated

--- controllers/notice.controller.js
+++ controllers/notice.controller.js
@@ -39,3 +39,34 @@ export const getNoticeById = async (req, res) => {
     res.status(500).json({ message: "Error fetching notice", error });
   }
 }
+
+// Update a notice by ID
+export const updateNoticeById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, link, date } = req.body;
+    const notice = await Notice.findOneAndUpdate({ id }, { title, description, link, date }, { new: true });
+    if (!notice) {
+      return res.status(404).json({ message: "Notice not found" });
+    }
+    res.status(200).json({noticeData: notice, message: "Notice updated successfully"});
+  } catch (error) {
+    res.status(500).json({ message: "Error updating notice", error });
+  }
+}
+
+// Delete a notice by ID
+export const deleteNoticeById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const notice = await Notice.findOneAndDelete({ id });
+    if (!notice) {
+      return res.status(404).json({ message: "Notice not found" });
+    }
+    res.status(200).json({ message: "Notice deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting notice", error });
+  }
+}
+
+
--- routes/notice.js
+++ routes/notice.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createNotice, getAllNotices } from "../controllers/notice.controller.js";
+import { createNotice, getAllNotices, updateNoticeById, deleteNoticeById } from "../controllers/notice.controller.js";
 
 
 const noticeRoutes = Router();
 
 noticeRoutes.post('/create-notice', createNotice);
 noticeRoutes.get('/get-notices', getAllNotices);
+noticeRoutes.put('/update-notice/:id', updateNoticeById);
+noticeRoutes.delete('/delete-notice/:id',deleteNoticeById);
 
 export default noticeRoutes;
